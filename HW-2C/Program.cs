bc6c185ff8525207475a4aa6c078933adef2da6d
j-naima0 <jannatulnaima519@gmail.com>
2025-05-24 20:31:11 -0400

    create HW-2C console menu application

--- HW-2C/Program.cs
+++ HW-2C/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace HW_2C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**************");
+            Console.WriteLine();
+            Console.WriteLine("Menu:");
+            Console.WriteLine();
+            Console.WriteLine("1 - New Game");
+            Console.WriteLine();
+            Console.WriteLine("2 - Load Game");
+            Console.WriteLine();
+            Console.WriteLine("3 - Options");
+            Console.WriteLine();
+            Console.WriteLine("4 - Quit");
+            Console.WriteLine();
+            Console.WriteLine("**************");
+        }
+    }
+}
